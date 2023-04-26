3c16ad7afac6adbfa7d09fc90c003ed14fc46e84
Jason Moon <noreply@jsnmoon.com>
2023-04-25 21:17:49 -0700

    Setup GitHub Actions for auto-publishing

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://lunar.haus',
   integrations: [mdx(), sitemap(), image({
     serviceEntryPoint: '@astrojs/image/sharp',
   })]
