838c5e537278e2497888ec6e89e56cec1958dc55
Jason Moon <noreply@jsnmoon.com>
2023-10-15 16:57:08 -0700

    Remove deprecated library

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,9 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 
-import image from "@astrojs/image";
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://lunar.haus',
   experimental: { assets: true },
-  integrations: [mdx(), sitemap(), image({
-    serviceEntryPoint: '@astrojs/image/sharp',
-  })]
+  integrations: [mdx(), sitemap()]
 });
